d80f66b61a988aa6d7c213b18e7439fcfac6889e
Syed Akbar Ali <akbarshigrie@gmail.com>
2026-03-08 21:45:59 +0500

    updated about section

--- src/components/Projects/project.jsx
+++ src/components/Projects/project.jsx
@@ -4,6 +4,14 @@ import { FaGithub } from "react-icons/fa";
 
 const projects = [
     {
+        title: "AI-Powered Website Chatbot - Customer Support",
+        description:
+            "Developed an AI-powered customer support chatbot using n8n automation and Google Gemini. The system reads company knowledge from structured documents, maintains conversation memory, and provides human-like responses. It also captures user contact details and automatically forwards leads and chat summaries to the support team via Gmail.",
+        tech: ["n8n", "AI Agents", "Google Gemini API", "LangChain", "Google Docs API", "Gmail API", "Workflow Automation", "Conversational AI"],
+        image: "/images/chatbot-ns.webp",
+        github: "https://github.com/akbarshigrie/AI-Powered-Website-Chatbot"
+    },
+    {
         title: "Plant Disease Detection using YOLOv4 (Deep Learning Object Detection)",
         description:
             "Implemented a high-accuracy plant leaf disease detection system using YOLOv4 and the PlantVillage dataset. Performed dataset preprocessing, augmentation, training, and evaluation, achieving strong multi-class performance for real-time agricultural disease identification.",
@@ -20,29 +28,20 @@ const projects = [
         github: "https://github.com/akbarshigrie/AI-Agent-System-for-E-commerce-Automation-n8n-Shopify-"
     },
     {
-        title: "Olympics Data Analysis & Visualization Dashboard",
-        description:
-            "A data-driven project exploring historical Olympic trends such as gold medal age distribution, female participation growth, and country-wise medal performance through interactive visualizations and insights.",
-        tech: ["Python", "Pandas", "NumPy", "Matplotlib", "Seaborn", "Gradio"],
-        image: "/images/olympics.jpg",
-        github: "https://github.com/akbarshigrie/Olympic-Data-Analysis"
-    },
-
-    {
-        title: "AI-Powered Website Chatbot - Customer Support",
-        description:
-            "Developed an AI-powered customer support chatbot using n8n automation and Google Gemini. The system reads company knowledge from structured documents, maintains conversation memory, and provides human-like responses. It also captures user contact details and automatically forwards leads and chat summaries to the support team via Gmail.",
-        tech: ["n8n", "AI Agents", "Google Gemini API", "LangChain", "Google Docs API", "Gmail API", "Workflow Automation", "Conversational AI"],
-        image: "/images/chatbot-ns.webp",
-        github: "https://github.com/akbarshigrie/AI-Powered-Website-Chatbot"
-    },
-    {
         title: "AI-Powered LeadEngine for Automated Outreach",
         description:
             "Built an AI-driven lead generation and outreach automation workflow using n8n. The system automatically organizes and enriches lead data, extracts verified email addresses and profile details, and generates personalized cold emails using an AI agent. This workflow eliminates manual data handling and repetitive email writing while enabling scalable, human-like outreach for business growth and marketing campaigns.",
         tech: ["n8n", "AI Agents", "OpenAI API", "Lead Data Enrichment", "Email Automation", "Workflow Automation"],
         image: "/images/linkedInDataScrapeWorkflow.webp",
         github: "https://github.com/akbarshigrie/AI-Powered-LeadEngine-for-Automated-Outreach"
+    },
+    {
+        title: "Olympics Data Analysis & Visualization Dashboard",
+        description:
+            "A data-driven project exploring historical Olympic trends such as gold medal age distribution, female participation growth, and country-wise medal performance through interactive visualizations and insights.",
+        tech: ["Python", "Pandas", "NumPy", "Matplotlib", "Seaborn", "Gradio"],
+        image: "/images/olympics.jpg",
+        github: "https://github.com/akbarshigrie/Olympic-Data-Analysis"
     },
 ];
 
